ffb92a2083488825f40e46f89d1358748a726944
frankynyny <frankynyny@hotmail.com>
2021-06-18 21:22:19 -0500

    Update TDProjects-Random_Num.py

--- TDProjects-Random_Num.py
+++ TDProjects-Random_Num.py
@@ -25,16 +25,24 @@ while True:
         print("\nYou got it! It took you {} tries.".format(str(tally)))
         if int(tally) > int(high_score):
             high_score = int(tally)
+        if int(tally) >= 10:
+            print("Lets just not speak about this again... I don't want you to feel SAT's are the only things holding you back.")
+        elif int(tally) < int(high_score):
+            print("Thanks very much for making the intelligence percentile harder to reach, now we all cant be #1 anymore.")
         try_again = (input("Would you like to play again?\n[y]es /[n]o: "))
         if try_again == "y":
             print("The HIGHSCORE is {}".format(high_score))
             random_num = random.randrange(1, 10)
+            tally = 0
             continue
         elif try_again == "n":
             break
+    elif user_input >= 11:
+        print("Listen, the plastic packet inside packages are not salt packets.... Please....stop eating them.")
+        continue
     elif user_input < random_num:
         tally += 1
-        print("It is higher!")
+        print("It is higher!\nTally: {}".format(tally))
         continue
     elif user_input > random_num:
         tally += 1
